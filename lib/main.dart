ed887dffb8bea9d8b95d81692d0d027adfbdcefa
Kayla Webb <kaylarwebb@gmail.com>
2019-12-26 09:44:43 -0500

    render news api data to the application

--- lib/main.dart
+++ lib/main.dart
@@ -9,29 +9,24 @@ import 'package:http/http.dart' as http;
 
 Future main() async {
   await DotEnv().load('.env');
-  runApp(MyApp());
+  runApp(NewsApp());
 }
 
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
+class NewsApp extends StatefulWidget {
+  NewsApp({ Key key }) : super(key: key);
 
   @override 
-  _MyAppState createState() => _MyAppState();
+  _NewsAppState createState() => _NewsAppState();
 }
 
 
-class _MyAppState extends State<MyApp> {
-  Future<NewsApiResponse> data;
-  List<Article> articles;
-  // final _newsArticles = List<Article>;
-
+class _NewsAppState extends State<NewsApp> {
+  Future<List<Article>> articles;
 
   @override
   void initState() {
     super.initState();
-    data = fetchPost();
-    // articles.add(Article.fromJson(json.decode(data));
-
+    articles = fetchArticles(http.Client());
   }
 
   @override 
@@ -48,20 +43,18 @@ class _MyAppState extends State<MyApp> {
         body: Center(
           child: Padding(
             padding: EdgeInsets.all(16.0),
-            child: FutureBuilder<NewsApiResponse>(
-              future: data,
+            child: FutureBuilder<List<Article>>(
+              future: articles,
               builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    padding: const EdgeInsets.all(12.0),
-                    itemBuilder: (context, i) {
-                      return Text('news is here');
+                if (snapshot.hasError) print(snapshot.error);
+                return snapshot.hasData
+                  ? ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                      return Text(snapshot.data[index].description);
                     },
-                  );
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
-                return CircularProgressIndicator();
+                  )
+                  : Center(child: CircularProgressIndicator());
               }
             )
           )
@@ -71,31 +64,32 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-/* API call */
 class NewsApiResponse {
   final String status;
   final int totalResults;
-  final List<Map> articles;
+  final List<dynamic> articles;
 
-  NewsApiResponse({ this.status, this.totalResults, this.articles });
+  NewsApiResponse({
+    this.status,
+    this.totalResults,
+    this.articles
+  });
 
-  factory NewsApiResponse.fromJson(Map<dynamic, dynamic> json) {
+  factory NewsApiResponse.fromJson(Map<String, dynamic> json) {
     return NewsApiResponse(
       status: json['status'],
       totalResults: json['totalResults'],
-      articles: json['articles']
-    );
+      articles: json['articles']);
   }
 }
 
 class Article {
-  final Map<String, Map> source;
+  final Map source;
   final String id;
   final String name;
   final String author;
   final String title;
   final String description;
-
 
   Article({ 
     this.source, 
@@ -106,7 +100,7 @@ class Article {
     this.description 
   });
 
-  factory Article.fromJson(Map<dynamic, dynamic> json) {
+  factory Article.fromJson(Map<String, dynamic> json) {
     return Article(
       source: json['source'],
       id: json['userId'],
@@ -118,19 +112,21 @@ class Article {
   }
 }
 
-Future<NewsApiResponse> fetchPost() async {
-  final apiKey = DotEnv().env['API_KEY'];
-  final response = await http.get(
-    'https://newsapi.org/v2/top-headlines?country=us',
-    headers: { "x-api-key": "$apiKey" }
-    );
-  print(response.body);
-  print(response.statusCode);
-      
-  if (response.statusCode == 200) {
-    return NewsApiResponse.fromJson(json.decode(response.body));
-  } else {
-    throw Exception('Failed to load post');
-  }
+List<Article> parseArticles(String responseBody) {
+  final parsedResponse = NewsApiResponse.fromJson(json.decode(responseBody));
+  return parsedResponse.articles.map<Article>((json) => Article.fromJson(json)).toList();
 }
 
+Future<List<Article>> fetchArticles(http.Client client) async {
+  final apiKey = DotEnv().env['API_KEY'];
+
+  final response = 
+    await client.get('https://newsapi.org/v2/top-headlines?country=us',
+    headers: { "x-api-key": "$apiKey" }
+    );
+  if (response.statusCode == 200) {
+    return parseArticles(response.body);
+  } else {
+    throw Exception('Failed to load news articles');
+  }
+}
